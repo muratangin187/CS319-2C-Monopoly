b30b16e4785f635ccf0de28a3305746338ece649
Ümit Yiğit Başaran <umityigitbsrn@gmail.com>
2020-12-01 13:46:44 +0300

    project app and select room are added again

---
+++ react/project_app/src/App.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import MainPage from "./pages/main_page/MainPage";
+import OptionPage from "./pages/option_page/OptionPage";
+import CreateRoomPage from "./pages/create_room_page/CreateRoomPage";
+
+function App() {
+  const [page, setPage] = useState("mainPage");
+
+  switch (page){
+    case "mainPage":
+      return(
+        <div className="App">
+          <MainPage setPage={setPage} />
+        </div>
+      );
+
+    case "optionPage":
+      return(
+          <div className="App">
+            <OptionPage setPage={setPage} />
+          </div>
+      );
+
+    case "createRoomPage":
+        return (
+            <div className="App">
+                <CreateRoomPage setPage={setPage}/>
+            </div>
+        );
+
+    default:
+      return (<div className="App">Default Empty Page</div>);
+  }
+}
+
+export default App;
---
+++ react/project_app/src/pages/create_room_page/components/CreateRoom.js
@@ -0,0 +1,95 @@
+import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {useFormik} from "formik";
+import BoardSelectionBox from "./BoardSelectionBox";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: "auto",
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    container: {
+        height: "88vh",
+        display: "flex",
+    },
+}));
+
+export default function CreateRoom() {
+    const formik = useFormik({
+        initialValues: {
+            room_name: "",
+            password: "",
+            selectedBoard: "",
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values))
+        }
+    });
+
+    const setSelectedBoard = (selectedBoard) => {
+        formik.values.selectedBoard = selectedBoard;
+    };
+
+    const classes = useStyles();
+
+    const [boards, setBoards] = useState(["Template-1", "Template-2"]);
+    return (
+        <Container className={classes.container} component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                    CREATE ROOM
+                </Typography>
+                <form className={classes.form} onSubmit={formik.handleSubmit}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        label="Room name"
+                        name="room_name"
+                        autoFocus
+                        onChange={formik.handleChange}
+                        value={formik.values.room_name}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
+                    <BoardSelectionBox boards={boards} setSelectedBoard={setSelectedBoard}/>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        CREATE
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}
