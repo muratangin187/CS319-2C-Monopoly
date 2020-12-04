10d57850a85ae571fb4b8106a98480a73b669f3b
Murat ANGIN <muratangin187@gmail.com>
2020-12-04 20:02:42 +0300

    Add Monopoly boilerplate for frontend and server

---
+++ server/main.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit("test", "MERHABALAR");
+    setTimeout(()=>{
+
+        socket.emit("test", "SELAM AQ");
+    },2000)
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
