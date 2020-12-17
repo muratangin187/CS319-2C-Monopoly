9c5db172d89281c5d5e96f3569280ede5ccac2cd
Emre YILDIZ <memreyildiz99@gmail.com>
2020-12-18 01:06:04 +0300

    Adjusted boards and all tiles to a paramater. Creating characters and move them.

--- monopoly/src/views/tileView/tileView.js
+++ monopoly/src/views/tileView/tileView.js
@@ -13,22 +13,22 @@ class tileView{
     }
 
     initializeDrawings(){
-        this.size = Globals.sizeOfBoard/11;
-        if (Math.floor(this.id/ 10) === 0){
-            this.x = Globals.sizeOfBoard-(this.id%10)*Globals.sizeOfBoard/11 - Globals.sizeOfBoard/11;
-            this.y = Globals.sizeOfBoard- Globals.sizeOfBoard/11;
+        this.size = Globals.sizeOfBoard/Globals.tileNumber;
+        if (Math.floor(this.id/ (Globals.tileNumber -1)) === 0){
+            this.x = Globals.sizeOfBoard-(this.id%(Globals.tileNumber -1))*Globals.sizeOfBoard/Globals.tileNumber - Globals.sizeOfBoard/Globals.tileNumber;
+            this.y = Globals.sizeOfBoard- Globals.sizeOfBoard/Globals.tileNumber;
         }
-        else if (Math.floor(this.id/ 10) === 1){
+        else if (Math.floor(this.id/ (Globals.tileNumber -1)) === 1){
             this.x = 0;
-            this.y = (Globals.sizeOfBoard- Globals.sizeOfBoard/11)-(this.id%10)*Globals.sizeOfBoard/11;
+            this.y = (Globals.sizeOfBoard- Globals.sizeOfBoard/Globals.tileNumber)-(this.id%(Globals.tileNumber -1))*Globals.sizeOfBoard/Globals.tileNumber;
         }
-        else if (Math.floor(this.id/ 10) === 2){
-            this.x = 0 + (this.id%10)*Globals.sizeOfBoard/11;
+        else if (Math.floor(this.id/ (Globals.tileNumber -1)) === 2){
+            this.x = 0 + (this.id%(Globals.tileNumber -1))*Globals.sizeOfBoard/Globals.tileNumber;
             this.y = 0;
         }
-        else if (Math.floor(this.id/ 10) === 3){
-            this.x = Globals.sizeOfBoard- Globals.sizeOfBoard/11;
-            this.y = 0 + (this.id%10)*Globals.sizeOfBoard/11;
+        else if (Math.floor(this.id/ (Globals.tileNumber -1)) === 3){
+            this.x = Globals.sizeOfBoard- Globals.sizeOfBoard/Globals.tileNumber;
+            this.y = 0 + (this.id%(Globals.tileNumber -1))*Globals.sizeOfBoard/Globals.tileNumber;
         }
         this.border.beginFill(0xCEE5D1);
         this.border.lineStyle(1, 0x333333);
