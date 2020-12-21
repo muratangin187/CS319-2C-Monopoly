7b91c5f069f966f79a314afc75c3609f62c0030b
Furkan Akkurt <furkanakkurt1905@gmail.com>
2020-12-22 01:10:09 +0300

    fixed branch merge issue. this is working now.

--- monopoly/src/views/cardView/stationCardView.js
+++ monopoly/src/views/cardView/stationCardView.js
@@ -1,5 +1,6 @@
 import CardView from "./cardView";
 import * as PIXI from 'pixi.js';
+import Globals from "../../globals";
 
 
 class StationCardView extends CardView{
@@ -20,17 +21,26 @@ class StationCardView extends CardView{
 
         let titleBackground = new PIXI.Graphics();
         titleBackground.name = "titleBackground";
-        titleBackground.beginFill(0x000000);
+        titleBackground.beginFill(0xf0f7f1);
         titleBackground.lineStyle(2, 0x333333);
         console.log(this.border);
         titleBackground.drawRect(this.border.x, this.border.y, this.border.width-2, this.border.height/5);
         titleBackground.alpha = 0.9;
         this.title.addChild(titleBackground);
 
+
+        let icon = new PIXI.Sprite(Globals.resources[this.station.image].texture);
+        icon.width = 50;
+        icon.height = 50;
+        icon.x = this.border.width / 2 - icon.width/2;
+        icon.y = titleBackground.y + 40;
+        this.card.addChild(icon);
+
         const style = new PIXI.TextStyle({
             fontFamily: "\"Times New Roman\", Times, serif",
+            fontSize : 11,
         });
-        this.titleText =new PIXI.Text(this.station.name, {...style, fill: "white"});
+        this.titleText =new PIXI.Text(this.station.name, {...style, align:"center"});
         this.titleText.name = "titleText";
         this.titleText.anchor.x =0.5;
         this.titleText.anchor.y =0.5;
@@ -42,17 +52,18 @@ class StationCardView extends CardView{
 If 1 owned ....................... ${this.station.rentPrice[1]}$
 If 2 owned ....................... ${this.station.rentPrice[2]}$
 If 3 owned ....................... ${this.station.rentPrice[3]}$`,{...style, fontSize: 11});
-        let costText = new PIXI.Text(`Mortgage Value ${this.station.mortgagePrice}`,{...style, align: "center", fontSize: 10});
+        let costText = new PIXI.Text(`Mortgage Value ${this.station.mortgagePrice}$`,{...style, align: "center", fontSize: 10});
 
-        propText.anchor.x =0.5;
-        propText.anchor.y =0.5;
-        propText.x = this.border.getBounds().x + this.border.getBounds().width / 2;
+        propText.anchor.x =0.6;
+        propText.anchor.y =0.6;
+        propText.x = this.border.x + this.border.width / 2 + 10;
         costText.anchor.x =0.5;
         costText.anchor.y =0.5;
-        costText.x = this.border.getBounds().x + this.border.getBounds().width / 2;
+        costText.x = this.border.x + this.border.width / 2;
 
-        propText.y = this.border.y + this.border.height/2;
-        costText.y = propText.y + this.border.height/4;
+
+        propText.y = this.border.y + 140;
+        costText.y = this.border.y + this.border.height + 12;
 
         this.content.addChild(propText);
         this.content.addChild(costText);
